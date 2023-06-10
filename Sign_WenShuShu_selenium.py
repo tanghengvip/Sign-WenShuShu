076fa237041792aed22e69b810b449c95f5361b4
tanghengvip <geeky@foxmail.com>
2023-06-10 21:54:30 +0800

    Update Sign_WenShuShu_selenium.py
    修复executable_path方法被启用导致脚本执行报错

--- Sign_WenShuShu_selenium.py
+++ Sign_WenShuShu_selenium.py
@@ -16,12 +16,13 @@ def send(push_token, title, text):
 
 def sign_wss(user, password, token, msgs : list):
     chrome_options = Options()
+    chrome_options.binary_location = "C:\Program Files\Google\Chrome\Application\chrome.exe"
     # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
     chrome_options.add_argument('--headless')
     # 以最高权限运行
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    b = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options)
+    b = webdriver.Chrome(options=chrome_options)
 
     b.get('https://www.wenshushu.cn/signin')
 
